3e8d161a1e88d5d100f2809dda0fb34b7c21fbc3
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-04-30 18:20:30 +0700

    update tugas modul 4 js

---
+++ modul4/javascript/eksternal_javascript.js
@@ -0,0 +1,9 @@
+ // Fungsi untuk menampilkan alert saat tombol diklik
+ function showAlert() {
+    alert("Terima kasih udah ngeklik dan bantu menyelesaikan tugasku!");
+}
+
+// Fungsi untuk mengubah teks paragraf
+function changeText() {
+    document.getElementById("text").innerText = "Saya Muhammad Rafli Adyatma dari Kelas 4G FTI Unmer Malang!";
+}
