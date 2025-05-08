8ed889c87296aaa371c66c38adde66d8364794a2
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-05-08 21:21:40 +0700

    nambahin modul 5

---
+++ modul5/First PHP.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>First PHP Muhammad Rafli Adyatma</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <?php 
+        echo "tulisan ini akan keluar karena echo";
+    ?>
+<p>apakah tulisan ini akan keluar</p>
+</body>
+</html>
